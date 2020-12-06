13d52acab5c590dff8bcf59df8ddb08f69c534c1
yd5618 <ysetily@gmail.com>
2020-12-06 19:24:31 +0000

    Trying to put everything in a POST method

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -1,11 +1,13 @@
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import java.awt.*;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.stream.Collectors;
 
-@WebServlet(urlPatterns={"/data"},loadOnStartup = 1) // check what this means exactly
+@WebServlet(urlPatterns={"/DBaccess"},loadOnStartup = 1) // check what this means exactly
 // specifies which servlet should be invoked for a url given by client?
 // specifies how servlet can be accessed basically - can be accessed through more URL patterns if needed
 
@@ -18,7 +20,7 @@ public class MainServlet extends HttpServlet {
         resp.getWriter().write("You have accessed the servlet");
         // req.getServletPath(); returns the ServletPath of the URL
         try {
-            processRequest(req, resp);
+            processGETRequest(req, resp);
         }
         catch (Exception e) {
             System.out.println("There was a problem");
@@ -28,10 +30,22 @@ public class MainServlet extends HttpServlet {
     // doPost will handle POST requests of our client & give appropriate response
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //
+        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+
+        // Connecting to the DB and querying what the POST request gave
+        Connection con = DBConnection.initialiseDB();
+        Statement s = con.createStatement();
+        // String sqlStr = "create table test (id SERIAL PRIMARY KEY,familyname varchar(128),givenname varchar(128),phonenumber varchar(32));";
+        s.executeUpdate(reqBody);
+        s.close();
+        con.close();
+
+        resp.setContentType("text/html");
+        resp.getWriter().write("You have successfully modified the DB");
     }
 
-    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    // Change the following to a post request
+    protected void processGETRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             Connection con = DBConnection.initialiseDB();
             Statement s = con.createStatement();
