df1fea1af43ecdda6cb41922e1caedab146564bd
yd5618 <ysetily@gmail.com>
2020-12-11 14:44:39 +0000

    trying POST

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -1,10 +1,10 @@
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import java.awt.*;
+// import java.awt.*;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Statement;
+// import java.sql.Connection;
+// import java.sql.Statement;
 import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns={"/DBaccess"},loadOnStartup = 1) // check what this means exactly
@@ -13,6 +13,8 @@ import java.util.stream.Collectors;
 
 public class MainServlet extends HttpServlet {
     // doGet will handle GET requests of our client & give appropriate response
+
+    /*
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // what do we do with req?
@@ -25,13 +27,14 @@ public class MainServlet extends HttpServlet {
         catch (Exception e) {
             System.out.println("There was a problem");
         }
-    }
+    } */
 
     // doPost will handle POST requests of our client & give appropriate response
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
 
+        /*
         try {
             // Connecting to the DB and querying what the POST request gave
             Connection con = DBConnection.initialiseDB();
@@ -43,14 +46,15 @@ public class MainServlet extends HttpServlet {
         }
         catch (Exception e) {
             System.out.println("There was a problem");
-        }
+        } */
 
         resp.setContentType("text/html");
-        resp.getWriter().write("You have successfully modified the DB");
+        resp.getWriter().write("You have successfully modified the DB - this was your request: "+reqBody);
     }
 
+    /*
     // Change the following to a post request
-    protected void processGETRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void processPOSTRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             Connection con = DBConnection.initialiseDB();
             Statement s = con.createStatement();
@@ -62,5 +66,5 @@ public class MainServlet extends HttpServlet {
         catch (Exception e) {
             System.out.println("There was a problem");
         }
-    }
+    } */
 }
