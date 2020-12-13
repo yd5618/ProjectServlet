918eb1f0f84ae917f3d9f839855992761faa0406
yd5618 <ysetily@gmail.com>
2020-12-13 13:46:48 +0000

    Changed a few issues regarding the return types and values of executeUpdate() and executeQuery() (don't return the same type)

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -3,10 +3,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// This class allows to connect to the DB on Heroku - static method 'initialiseDB()' returns a Connection object
 public class DBConnection {
-    public static Connection initialiseDB()
-            throws URISyntaxException, SQLException, ClassNotFoundException
-    {
+    public static Connection initialiseDB() throws URISyntaxException, SQLException, ClassNotFoundException {
         String dbDriver = "org.postgresql.Driver";
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
         Class.forName(dbDriver);
--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -1,21 +1,40 @@
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import java.awt.*;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.stream.Collectors;
 
-@WebServlet(urlPatterns={"/DBaccess"},loadOnStartup = 1) // check what this means exactly
-// specifies which servlet should be invoked for a url given by client?
-// specifies how servlet can be accessed basically - can be accessed through more URL patterns if needed
+// Line below specifies how the servlet can be accessed basically ('/DBaccess') - can be accessed through more URL patterns if needed
+@WebServlet(urlPatterns={"/DBaccess"},loadOnStartup = 1)
 
 public class MainServlet extends HttpServlet {
-    // doGet will handle GET requests of our client & give appropriate response
+    // doGet will handle GET requests of our client & give appropriate response (retrieve information from the DB) - change this
+    // doPost will handle POST requests of our client & give appropriate response (add something to the DB/modify something in the DB)
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Get body of the request - this is not good practice => find another way to do this - GET should only return information specified by URI/URL
+        // https://stackoverflow.com/questions/978061/http-get-with-request-body --> this gives more information on this
+        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+
+        try {
+            // Connecting to the DB and querying what the POST request gave
+            Connection con = DBConnection.initialiseDB();
+            Statement s = con.createStatement();
+            ResultSet rset = s.executeQuery(reqBody); // a ResultSet object is a table of data representing a database
+            while(rset.next()) {
+                System.out.println(rset.getString("brand"));
+            }
+            s.close();
+            con.close();
+        }
+        catch (Exception e) {
+            System.out.println("There was a problem");
+        }
+
         // what do we do with req?
         resp.setContentType("text/html");
         resp.getWriter().write("You have accessed the servlet");
@@ -29,16 +48,17 @@ public class MainServlet extends HttpServlet {
         } */
     }
 
-    // doPost will handle POST requests of our client & give appropriate response
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Get the request body (SQL query to be executed)
         String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        int nb;
 
         try {
             // Connecting to the DB and querying what the POST request gave
             Connection con = DBConnection.initialiseDB();
             Statement s = con.createStatement();
-            s.executeUpdate(reqBody);
+            nb = s.executeUpdate(reqBody); // will it actually return an int with the nb of rows affected?
             s.close();
             con.close();
         }
@@ -47,22 +67,6 @@ public class MainServlet extends HttpServlet {
         }
 
         resp.setContentType("text/html");
-        resp.getWriter().write("You have successfully modified the DB - this was your request: "+reqBody);
+        resp.getWriter().write("You have successfully modified the DB - this was your request: "+reqBody+" and you affected "+nb+" rows");
     }
-
-    /*
-    protected void Connect(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            Connection con = DBConnection.initialiseDB();
-            Statement s = con.createStatement();
-            String sqlStr = "insert into products (brand,nime,amount,sprice,pprice,fullstock,limitation) values('Vicks','Vaporub','100 g',4.5,3.7,15,TRUE);";
-            s.executeUpdate(sqlStr);
-            s.close();
-            con.close();
-        }
-        catch (Exception e) {
-            System.out.println("There was a problem");
-        }
-    }
-     */
 }
