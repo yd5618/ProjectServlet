c251abb6f6f9f6e07ed5051175d1ca3c86d09b9e
yd5618 <ysetily@gmail.com>
2020-12-27 16:22:28 +0100

    Trying GET method

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -11,24 +11,25 @@ import java.util.stream.Collectors;
 @WebServlet(urlPatterns={"/DBaccess"},loadOnStartup = 1)
 
 public class MainServlet extends HttpServlet {
-    // doGet will handle GET requests of our client & give appropriate response (retrieve information from the DB) - change this
+    // doGet will handle GET requests of our client & give appropriate response (retrieve information from the DB)
     // doPost will handle POST requests of our client & give appropriate response (add something to the DB/modify something in the DB)
 
-    /*
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // Get body of the request - this is not good practice => find another way to do this - GET should only return information specified by URI/URL
         // https://stackoverflow.com/questions/978061/http-get-with-request-body --> this gives more information on this
-        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+
+        String query = "SELECT * FROM products;";
 
         try {
-            // Connecting to the DB and querying what the POST request gave
+            // Connecting to the DB and returning what is identified by the URL
             Connection con = DBConnection.initialiseDB();
             Statement s = con.createStatement();
-            ResultSet rset = s.executeQuery(reqBody); // a ResultSet object is a table of data representing a database
+            ResultSet rset = s.executeQuery(query); // a ResultSet object is a table of data representing a database
             while(rset.next()) {
-                System.out.println(rset.getString("brand"));
+                System.out.println(rset.getString("brand")+" "+rset.getString("id"));
             }
+            rset.close();
             s.close();
             con.close();
         }
@@ -40,14 +41,7 @@ public class MainServlet extends HttpServlet {
         resp.setContentType("text/html");
         resp.getWriter().write("You have accessed the servlet");
         // req.getServletPath(); returns the ServletPath of the URL
-
-        try {
-            processGETRequest(req, resp);
-        }
-        catch (Exception e) {
-            System.out.println("There was a problem");
-        }
-    } */
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
